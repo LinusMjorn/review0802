99cdcb7ce1b0431aab7ff779ee00727b927ccc55
Linus Mjornstedt <linusmjorn@linuss-mbp-2.home>
2021-02-09 14:11:00 +0000

    first commit, first test written

---
+++ spec/scores_spec.rb
@@ -0,0 +1,7 @@
+describe Scores do
+    describe 'count' do
+        it 'returns Green: 1 when a score of 89 is entered do'
+        score = Scores.new
+        expect(score.count(89)).to eq "Green: 1"
+    end
+end
